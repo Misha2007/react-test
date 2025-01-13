5fb8d11f7d7648af3536bac11fbdc500f4238bcf
Misha2007 <mishadrogovoz@gmail.com>
2025-01-13 14:08:21 +0200

    Ülesanne 6 - Button.js komponendi lisamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,3 +1,5 @@
+import Button from "./UI/Button";
+
 const MealItem = (props) => {
   const price = new Intl.NumberFormat("en-IN", {
     style: "currency",
@@ -16,7 +18,7 @@ const MealItem = (props) => {
           <p className="meal-item-description">{props.meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+const Button = (props) => {
+  const buttonClass = props.textonly ? "text-button" : "button";
+  return (
+    <button className={buttonClass} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
