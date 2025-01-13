6a812be22fb82e26676288464e93adfbd601e039
Misha2007 <mishadrogovoz@gmail.com>
2025-01-13 13:48:45 +0200

    Ülesanne 4 - kuva saadud andmed MealItem komponendi abil

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
   const [data, setData] = useState([]);
@@ -16,9 +17,9 @@ const Meals = () => {
   console.log(data);
   return (
     <ul id="meals">
-      {
-        // list of meals
-      }
+      {data.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
     </ul>
   );
 };
